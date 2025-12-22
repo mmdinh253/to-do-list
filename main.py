18065da4a7fee4707482081f18d25235308ff5ca
Michael Dinh <mdinh253@cs.washington.edu>
2025-12-21 22:45:47 -0800

    inital practice

---
+++ main.py
@@ -0,0 +1,61 @@
+# array of tasks
+tasks = []
+
+def addTask():
+    task = input("Please enter a task: ")
+    tasks.append(task)
+    ### f in front of the string so we can insert variables into it
+    print(f"Task '{task}' added to the list.")
+
+def listTasks():
+    if not tasks:
+        print("There are no tasks currently")
+    else:
+        print("Current Tasks: ")
+        for index, task in enumerate(tasks):
+            ### ex. "Task #1. Take out the groceries"
+            print(f"Task #{index}. {task}")
+
+def deleteTask():
+    listTasks()
+    try:
+        ### casting a task to an int in the array
+        taskToDelete = int(input("Enter the # to delete: "))
+        if taskToDelete >=0 and taskToDelete < len(tasks):
+            tasks.pop(taskToDelete)
+            print(f"Task {taskToDelete} has been removed")
+        else:
+            print(f"Task #{taskToDelete} was not found")
+    except:
+        print("Invalid input.")
+
+if __name__ == "__main__":
+    ### Creating a loop to run app
+    print("Welcome to the to do list app!")
+    ### Always running frequently until we tell it to stop
+    while True:
+        print("\n")
+        print("Please select one of the following options:")
+        print("------------------------------------------")
+        print("1. Add a new task")
+        print("2. Delete a task")
+        print("3. Lists tasks")
+        print("4. Quit")
+
+        ### user input
+        choice = input("Enter your choice: ")
+
+        if(choice == "1"):
+            addTask()
+        elif(choice == "2"):
+            deleteTask()
+        elif(choice == "3"):
+            listTasks()
+        elif(choice == "4"):
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+print("Done")
+
+
